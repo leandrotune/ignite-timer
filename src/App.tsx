7707ae07ab0c8a031fbf5c6541e88a203428116c
Leandro Antunes <lele.tune@gmail.com>
2022-07-10 20:24:43 -0300

    Estilos globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { ThemeProvider } from "styled-components"
 import { Button } from './components/Button'
+import { GlobalStyle } from "./styles/global"
 import { DefaultTheme } from "./styles/theme/default"
 
 export function App() {
@@ -10,6 +11,7 @@ export function App() {
       <Button variant='danger' />
       <Button variant='sucess' />
       <Button />
+      <GlobalStyle />
     </ThemeProvider>
   )
 }
