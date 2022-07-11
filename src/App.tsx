412a7dbf2133f0a222147e357972ba88c7acd1b4
Leandro Antunes <lele.tune@gmail.com>
2022-07-11 11:24:41 -0300

    React Router DOM

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
+import { Router } from './components/Router'
+import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
-import { Button } from './components/Button'
 import { GlobalStyle } from './styles/global'
-import { defaultTheme } from './styles/theme/default'
+import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="danger" />
-      <Button variant="sucess" />
-      <Button />
-      <GlobalStyle />
+      <BrowserRouter>
+        <Router />
+        <GlobalStyle />
+      </BrowserRouter>
     </ThemeProvider>
   )
 }
