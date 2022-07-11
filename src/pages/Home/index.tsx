157c2025eb58556ff1413839ea507e217537c577
Leandro Antunes <lele.tune@gmail.com>
2022-07-11 17:23:43 -0300

    Página: Home

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,33 @@
+import { Play } from 'phosphor-react'
+import { CountDownContainer, FormContainer, HomeContainer, Separator } from './styled'
+
+export function Home() {
+  return (
+    <HomeContainer>
+      <form>
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <input id="task" />
+
+          <label htmlFor="">durante</label>
+          <input type="number" id="minuteAmount" />
+
+          <span>minutos.</span>
+        </FormContainer>
+
+        <CountDownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountDownContainer>
+
+        <button type="submit">
+          <Play size={24} />
+          Começar
+        </button>
+      </form>
+    </HomeContainer>
+  )
+}
