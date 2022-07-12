42cfb2bbe4c844474bc885232f84717c7004728f
Leandro Antunes <lele.tune@gmail.com>
2022-07-12 19:15:27 -0300

    Componnete: Status

--- src/pages/Histoty/index.tsx
+++ src/pages/Histoty/index.tsx
@@ -1,4 +1,4 @@
-import { HistoryContainer, HistoryList } from './styles'
+import { HistoryContainer, HistoryList, Status } from './styles'
 
 export function History() {
   return (
@@ -20,43 +20,57 @@ export function History() {
               <td>Tarefa</td>
               <td>20 minutos</td>
               <td>há dois meses</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor="yellow">em andamento</Status>
+              </td>
             </tr>
             <tr>
               <td>Tarefa</td>
               <td>20 minutos</td>
               <td>há dois meses</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor="green">concluído</Status>
+              </td>
             </tr>
             <tr>
               <td>Tarefa</td>
               <td>20 minutos</td>
               <td>há dois meses</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor="green">concluído</Status>
+              </td>
             </tr>
             <tr>
               <td>Tarefa</td>
               <td>20 minutos</td>
               <td>há dois meses</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor="green">concluído</Status>
+              </td>
             </tr>
             <tr>
               <td>Tarefa</td>
               <td>20 minutos</td>
               <td>há dois meses</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor="green">concluído</Status>
+              </td>
             </tr>
             <tr>
               <td>Tarefa</td>
               <td>20 minutos</td>
               <td>há dois meses</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor="red">interrompido</Status>
+              </td>
             </tr>
             <tr>
               <td>Tarefa</td>
               <td>20 minutos</td>
               <td>há dois meses</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor="yellow">em andamento</Status>
+              </td>
             </tr>
           </tbody>
         </table>
